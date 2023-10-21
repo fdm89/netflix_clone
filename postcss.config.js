961e79aa35649382aaa130e26ca737c5dc214da9
fulvio di maio <fulvio1989@gmail.com>
2023-10-21 10:33:00 +0200

    first commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  content:[
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme:{
+    extend: {}
+  },
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
